8034e3bf0354f51f59d8e710ea72e99323a53486
NazJetpack <41399000+naze93@users.noreply.github.com>
2023-04-29 20:13:07 +0200

    Update MyApp.kt

--- app/src/main/java/com/example/leparisien/ui/MyApp.kt
+++ app/src/main/java/com/example/leparisien/ui/MyApp.kt
@@ -1,1 +1,37 @@
-package com.example.leparisien.uiimport androidx.compose.foundation.layout.Columnimport androidx.compose.foundation.layout.fillMaxSizeimport androidx.compose.foundation.layout.paddingimport androidx.compose.material.Scaffoldimport androidx.compose.runtime.Composableimport androidx.compose.ui.Modifierimport androidx.navigation.compose.rememberNavControllerimport com.example.leparisien.Rimport com.example.leparisien.ui.components.BottomNavBarimport com.example.leparisien.ui.components.MyTopAppBarimport com.example.leparisien.ui.screens.NavGraph//test commit@Composablefun MyApp() {    // 2. Scaffold pour y mettre MyTopAppBar et BottomMenu    val navController = rememberNavController()    Scaffold(        topBar = {            MyTopAppBar(                image = R.drawable.leparisien,                onButtonClick = {}            )        },        bottomBar = { BottomNavBar(navController)}        ) { paddingValues ->        Column(            modifier = Modifier                .fillMaxSize()                .padding(paddingValues)        ) {            NavGraph(navController = navController)        }    }}
+package com.example.leparisien.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.example.leparisien.R
+import com.example.leparisien.ui.components.BottomNavBar
+import com.example.leparisien.ui.components.MyTopAppBar
+import com.example.leparisien.ui.screens.NavGraph
+//test commit
+@Composable
+fun MyApp() {
+    // 2. Scaffold pour y mettre MyTopAppBar et BottomMenu
+    val navController = rememberNavController()
+    Scaffold(
+        topBar = {
+            MyTopAppBar(
+                image = R.drawable.leparisien,
+                onButtonClick = {}
+            )
+        },
+        bottomBar = { BottomNavBar(navController)}
+        ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            NavGraph(navController = navController)
+        }
+    }
+}
+
